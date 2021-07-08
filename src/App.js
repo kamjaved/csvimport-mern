9a82f5a7a714e7d914818485c9decc1e86029124
Kamran Javed <kamranjaved1234@gmail.com>
2021-07-08 13:01:00 +0530

    final code react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+  const [file, setFile] = useState();
+
+  // On file select 
+  const onFileChange = e => {
+    setFile(e.target.files[0]);
+    // alert('fileSelected', file)
+    console.log(e.target.files[0]);
+
+  };
+
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+
+    let formData = new FormData();
+    formData.append("file", file);
+
+    console.log("FILE", file);
+    console.log("FORMDTA", formData);
+
+    axios.post('http://localhost:5000/upload', formData, {
+      header: {
+        "Content-Type": "multipart/form-data",
+      }
+    })
+
+      .then(response => {
+        console.log("Status: ", response.status);
+        alert(response.data);
+
+      }).catch(error => {
+        console.error('Something went wrong!', error);
+      });
+
+    setFile();
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>CSV IMPORT FILE</h1>
+
+        <form onSubmit={e => onSubmitHandler(e)} encType='multipart/form-data'>
+          <input
+            type="file" name="file"
+            accept='*.csv'
+            onChange={onFileChange}
+          />
+          <button type="submit">UPLOAD</button>
+        </form>
+
       </header>
     </div>
   );
